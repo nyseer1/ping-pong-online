83d86f6483d793344a9faad0c78336c749ed0d85
Nagi Williams <nagi132@gmail.com>
2023-10-07 00:24:17 -0400

    added title to image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="App-background"></div>
       <header className="App-header">
+        <img src="/images/title-image.png" alt="Game Title" className="title-image" />
       </header>
     </div>
   );
