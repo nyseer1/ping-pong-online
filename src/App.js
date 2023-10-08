ad566277f39117caf11ba0b1538eb5e077484fd0
Savannah Chhann <schhann327@gmail.com>
2023-10-08 17:28:09 -0400

    added TitleScreen component and modified code accordingly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import TitleScreen from './TitleScreen';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-background"></div>
-      <header className="App-header">
-        <img src="/images/title-image.png" alt="Game Title" className="title-image" />
-      </header>
+      <TitleScreen/>
     </div>
   );
 }
---
+++ src/TitleScreen.js
@@ -0,0 +1,13 @@
+import './TitleScreen.css';
+
+function TitleScreen () {
+  return (
+    <div className="App-background">
+      <header className="App-header">
+        <img src="/images/title-image.png" alt="Game Title" className="title-image" />
+      </header>
+      </div>
+  );
+}
+
+export default TitleScreen;
